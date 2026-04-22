df685eee05952154f3863f6579aa5773da8663af
Mohammad Aaryan Hussain <141989940+Arnoldschawangxzerx@users.noreply.github.com>
2026-04-22 18:18:04 +0300

    I improved the UI
    I made the basic UI of a game
    Plus added popup logic for NewGame.

---
+++ Mobile_Game_1/Assets/Scripts/UIManager.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class UIManager : MonoBehaviour
+{
+    public GameObject newGamePopup;
+
+    
+   
+
+public void StartNewGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+    public void OnPlayClicked()
+    {
+        Debug.Log("Loading Game Scene...");
+        SceneManager.LoadScene("GameScene");
+    }
+    public void OnNewGameClicked()
+    {
+        Debug.Log("Loading Settings Scene...");
+        SceneManager.LoadScene("SettingsScene");
+    }
+    
+     public void OpenPopup()
+    {
+        newGamePopup.SetActive(true);
+    }
+
+    public void ClosePopup()
+    {
+        newGamePopup.SetActive(false);
+    }
+
+    public void OnLoadClicked()
+    {
+        Debug.Log("Loading Settings Scene...");
+        SceneManager.LoadScene("SaveScene");
+    }
+    public void OnSettingsClicked()
+    {
+        Debug.Log("Loading Settings Scene...");
+        SceneManager.LoadScene("SettingsScene");
+    }
+    public void OnQuitClicked()
+    {
+        Debug.Log("Loading Settings Scene...");
+        SceneManager.LoadScene("QuitScene");
+    }
+}
